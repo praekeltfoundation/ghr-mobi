96d84992e0dccd5bf360d98124a9092840b2bcea
Simon de Haan <simon@praekeltfoundation.org>
2013-12-10 10:54:32 +0200

    pep8 app app

--- src/app/articles/models.py
+++ src/app/articles/models.py
@@ -8,10 +8,10 @@ from django.db import models
 from tunobase.core import models as core_models
 from tunobase.poll import models as poll_models
 
+
 class Article(core_models.ContentModel):
     poll = models.ForeignKey(
-        poll_models.PollQuestion, 
-        blank=True, 
+        poll_models.PollQuestion,
+        blank=True,
         null=True
     )
-
--- src/app/articles/views.py
+++ src/app/articles/views.py
@@ -12,33 +12,36 @@ from tunobase.core import utils as core_utils
 from app.root import views as root_views
 from app.articles import models
 
+
 class Articles(generic_views.ListView):
-    
+
     def get_queryset(self):
         return models.Article.objects.permitted()
-    
+
+
 class ArticleDetail(root_views.CommentListDetail):
-    
+
     def get_object(self):
         return core_utils.get_permitted_object_or_404(
-            models.Article, 
+            models.Article,
             slug=self.kwargs['slug']
         )
-        
+
+
 class PostComment(root_views.PostComment):
-    
+
     def get_context_data(self, **kwargs):
         context = super(PostComment, self).get_context_data(**kwargs)
         obj = core_utils.get_permitted_object_or_404(
-            models.Article, 
+            models.Article,
             slug=self.kwargs['slug']
         )
-        content_type_id= ContentType.objects.get_for_model(obj).id
-        
+        content_type_id = ContentType.objects.get_for_model(obj).id
+
         context.update({
             'content_type_id': content_type_id,
             'object': obj,
             'next': reverse('article_detail', kwargs=self.kwargs)
         })
-        
+
         return context
