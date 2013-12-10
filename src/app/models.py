54a5d61abc4bb8266cd795952b221f69d3957c28
Simon de Haan <simon@praekeltfoundation.org>
2013-12-10 10:06:20 +0200

    pep8 app/models.py

--- src/app/models.py
+++ src/app/models.py
@@ -12,23 +12,24 @@ from tunobase.poll import models as poll_models
 from app.research_tool import models as research_tool_models
 from app.discussions import models as discussion_models
 
+
 class SitePreferences(Preferences):
     __module__ = 'preferences.models'
-    
+
     active_poll = models.ForeignKey(
-        poll_models.PollQuestion, 
-        related_name='active_polls', 
-        blank=True, 
+        poll_models.PollQuestion,
+        related_name='active_polls',
+        blank=True,
         null=True
     )
     active_discussion = models.ForeignKey(
-        discussion_models.Discussion, 
-        related_name='active_discussions', 
-        blank=True, 
+        discussion_models.Discussion,
+        related_name='active_discussions',
+        blank=True,
         null=True
     )
     research_tool = models.ForeignKey(
-        research_tool_models.ResearchTool, 
-        blank=True, 
+        research_tool_models.ResearchTool,
+        blank=True,
         null=True
     )
