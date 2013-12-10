2480fcb2b7df79faa11d568428d1d9f6c08eb93f
Simon de Haan <simon@praekeltfoundation.org>
2013-12-10 11:55:52 +0200

    galleries pep8

--- src/app/galleries/views.py
+++ src/app/galleries/views.py
@@ -11,42 +11,45 @@ from tunobase.core import utils as core_utils, models as core_models
 
 from app.root import views as root_views
 
+
 class Galleries(generic_views.ListView):
-    
+
     def get_queryset(self):
         return core_models.Gallery.objects.permitted()
-    
+
+
 class GalleryDetail(root_views.CommentListDetail):
-    
+
     def get_object(self):
         return core_utils.get_permitted_object_or_404(
-            core_models.Gallery, 
+            core_models.Gallery,
             slug=self.kwargs['slug']
         )
-        
+
+
 class GalleryImageDetail(root_views.CommentListDetail):
-    
+
     def get_object(self):
         return core_utils.get_permitted_object_or_404(
-            core_models.GalleryImage, 
+            core_models.GalleryImage,
             pk=self.kwargs['pk']
         )
-    
+
+
 class PostComment(root_views.PostComment):
-    
+
     def get_context_data(self, **kwargs):
         context = super(PostComment, self).get_context_data(**kwargs)
         obj = core_utils.get_permitted_object_or_404(
-            core_models.Gallery, 
+            core_models.Gallery,
             slug=self.kwargs['slug']
         )
-        content_type_id= ContentType.objects.get_for_model(obj).id
-        
+        content_type_id = ContentType.objects.get_for_model(obj).id
+
         context.update({
             'content_type_id': content_type_id,
             'object': obj,
             'next': reverse('gallery_detail', kwargs=self.kwargs)
         })
-        
+
         return context
-        
