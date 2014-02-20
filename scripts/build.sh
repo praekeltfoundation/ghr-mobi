a2e8d90002616cf36ea73e0231b779bbdc35e846
Colin Alston <colin.alston@gmail.com>
2014-02-20 15:08:27 +0200

    Fix directories in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 cp -a ghr-mobi/src ./build/ghr-mobi
-mkdir -p ./build/ghr-mobi/media/uploads
-touch ./build/ghr-mobi/media/uploads/Keep
+
+mkdir -p ./build/ghr-mobi/project/media/uploads
+mkdir -p ./build/ghr-mobi/project/whoosh
+mkdir -p ./build/ghr-mobi/project/static
+
+touch ./build/ghr-mobi/project/media/uploads/Keep
+touch ./build/ghr-mobi/project/whoosh/Keep
+touch ./build/ghr-mobi/project/static/Keep
 
 ${PIP} install -r ghr-mobi/requirements.txt
 
