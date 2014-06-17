ea76d12c1cb3e71cdb7b36c707498e947803dca4
Milton Madanda <madandat@gmail.com>
2014-06-17 19:09:53 +0200

    fix tunobase version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     dependency_links = [
         'https://github.com/unomena/django-photologue/tarball/2.8.praekelt#egg=django-photologue-2.8.praekelt',
         'https://github.com/unomena/django-ckeditor-new/tarball/3.6.2.2#egg=django-ckeditor-3.6.2.2',
-        'https://github.com/unomena/tunobase/tarball/1.0.9#egg=tunobase-1.0.9',
+        'https://github.com/unomena/tunobase/tarball/1.0.10#egg=tunobase-1.0.10',
         'https://github.com/unomena/django-holodeck/tarball/0.1.2#egg=django-holodeck-0.1.2',
         'https://github.com/unomena/photon/tarball/0.0.6#egg=photon-0.0.6',
     ],
