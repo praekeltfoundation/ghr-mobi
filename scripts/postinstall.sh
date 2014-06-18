2e3bfd6037f73725405cf835bc60c2c18fc274e1
Milton Madanda <madandat@gmail.com>
2014-06-18 10:26:19 +0200

    update requirements for sideloader

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,8 +1,11 @@
 manage="${VENV}/bin/python ${INSTALLDIR}/ghr-mobi/manage.py"
+pip="${VENV}/bin/pip"
 
-pip install http://github.com/unomena/django-ckeditor-new/tarball/3.6.2.2#egg=django-ckeditor-3.6.2.2
-pip install http://github.com/unomena/django-photologue/tarball/2.8.praekelt#egg=django-photologue-2.8.praekelt
-pip install http://github.com/unomena/tunobase/tarball/1.0.9#egg=tunobase-1.0.9
+$pip install http://github.com/unomena/django-ckeditor-new/tarball/3.6.2.2#egg=django-ckeditor-3.6.2.2
+$pip install http://github.com/unomena/django-photologue/tarball/2.8.praekelt#egg=django-photologue-2.8.praekelt
+$pip install http://github.com/unomena/tunobase/tarball/1.0.10#egg=tunobase-1.0.10
+$pip install http://github.com/unomena/django-holodeck/tarball/0.1.2#egg=django-holodeck-0.1.2
+$pip install http://github.com/unomena/photon/tarball/0.0.6#egg=photon-0.0.6
 
 $manage syncdb --noinput --no-initial-data --migrate
 $manage collectstatic --noinput
