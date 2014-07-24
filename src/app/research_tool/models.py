e27fba913c9bb01d01aba146e869d4ee3b3529bb
JeyabalaMurugan <jeyabalp@techaffinity.com>
2014-07-24 22:25:30 +0530

    Removed commented code from models

--- src/app/research_tool/models.py
+++ src/app/research_tool/models.py
@@ -9,9 +9,6 @@ from tunobase.core import models as core_models
 from tunobase.poll import models as poll_models
 
 class ResearchTool(core_models.ContentModel):
-    # poll = models.OneToOneField(
-    #     poll_models.PollQuestion
-    # )
     polls = models.ManyToManyField(
         poll_models.PollQuestion
     )
