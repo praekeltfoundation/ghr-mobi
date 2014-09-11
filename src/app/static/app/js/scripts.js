5056aa5afd90a0170b8e8bef6e353a1c299d3ebc
karthikeyan <karthikeyanu@techaffinty.com>
2014-09-11 20:46:00 +0530

    Removed pagination javascript and design issues

--- src/app/static/app/js/scripts.js
+++ src/app/static/app/js/scripts.js
@@ -1,12 +1,3 @@
 $(document).ready(function(){
-	$('.add_comment').click(function(e){
-		$('#frmComment').removeClass('hidden');
-		
-		return false;
-	});
-	
-	/* Progressive enhancement stuff */
-	$('.poll_results').removeClass('hidden');
-	$('.ajax_pagination_container').removeClass('hidden');
 	$('.social_media_share_widget').removeClass('hidden');
 });
