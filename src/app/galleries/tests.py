338f16be90441290008d31e11a744be1969dc885
karthikeyan <karthikeyanu@techaffinty.com>
2014-09-23 21:47:32 +0530

    Added test cases for gallery

--- src/app/galleries/tests.py
+++ src/app/galleries/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
+from tunobase.core import utils as core_utils, constants\
+    as core_constants, models as core_models
 from django.template.defaultfilters import slugify
-from tunobase.core import utils as core_utils,constants as core_constants, models as core_models
+from django.template import Template, Context
+
 
 class GalleryModelTestCase(TestCase):
     title = 'Gallery Model Title'
+    image_name = 'MyImage'
     slug = slugify(title)
 
     def setUp(self):
@@ -13,7 +17,12 @@ class GalleryModelTestCase(TestCase):
         self.gallery_object = core_models.Gallery.objects.create(
             title=self.title,
         )
-
+        for image_no in range(3):
+            
+            self.gallery_image_object = self.gallery_object.images.create(
+                image_name='%s %i' % (self.image_name, image_no),
+            )
+            
     def test_gallery_model(self):
         '''
         Test that the Gallery Model was created and has
@@ -21,3 +30,11 @@ class GalleryModelTestCase(TestCase):
         '''
         gallery_object = core_models.Gallery.objects.get(slug=self.slug)
         self.assertEqual(gallery_object.state, core_constants.STATE_PUBLISHED)
+
+    def test_get_count_value(self):
+        gallery_object = core_models.Gallery.objects.get(slug=self.slug)
+        gallery_image_object = gallery_object.images.all()[0]
+        t = Template('{% load gallery_tags %}{% getCountValue gallery_object gallery_image_object as count_value %}')
+        c = Context({"gallery_object": self.gallery_object, "gallery_image_object": self.gallery_image_object})
+        t.render(c)
+        self.assertEqual(c['count_value'], {'current_image': 2, 'total_count': 3})
--- src/app/root/templatetags/gallery_tags.py
+++ src/app/root/templatetags/gallery_tags.py
@@ -1,5 +1,6 @@
 """
-    This module is used for display the image numbers with total image numbers
+    This module is used for display the 
+    image numbers with total image numbers
 """
 from django import template
 register = template.Library()
@@ -7,7 +8,7 @@ register = template.Library()
 
 @register.assignment_tag
 def getCountValue(gallery, gallery_image_pk):
-    gallery_images = list(gallery.images.all().reverse())
+    gallery_images = tuple(gallery.images.all().reverse())
     return {
         'current_image': gallery_images.index(gallery_image_pk)+1,
         'total_count': len(gallery_images)
