885a6c2949216a47c19b5f74f7399c3c88cda653
karthikeyan <karthikeyanu@techaffinty.com>
2014-09-26 19:31:07 +0530

    Modified test case

--- src/app/galleries/tests.py
+++ src/app/galleries/tests.py
@@ -17,7 +17,7 @@ class GalleryModelTestCase(TestCase):
         self.gallery_object = core_models.Gallery.objects.create(
             title=self.title,
         )
-        for image_no in range(3):
+        for image_no in range(4):
             
             self.gallery_image_object = self.gallery_object.images.create(
                 image_name='%s %i' % (self.image_name, image_no),
@@ -33,8 +33,8 @@ class GalleryModelTestCase(TestCase):
 
     def test_get_count_value(self):
         gallery_object = core_models.Gallery.objects.get(slug=self.slug)
-        gallery_image_object = gallery_object.images.all()[0]
+        gallery_image_object = gallery_object.images.all()[1]
         t = Template('{% load gallery_tags %}{% getCountValue gallery_object gallery_image_object as count_value %}')
         c = Context({"gallery_object": self.gallery_object, "gallery_image_object": self.gallery_image_object})
         t.render(c)
-        self.assertEqual(c['count_value'], {'current_image': 2, 'total_count': 3})
+        self.assertEqual(c['count_value'], {'current_image': 2, 'total_count': 4})
