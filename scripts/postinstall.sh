4a610e186dc3676678d2f7cd479e09c242f26253
Milton Madanda <madandat@gmail.com>
2014-10-14 13:03:42 +0200

    ensure correct django settings are used

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -7,5 +7,5 @@ $pip install http://github.com/unomena/tunobase/tarball/1.0.10#egg=tunobase-1.0.
 $pip install http://github.com/unomena/django-holodeck/tarball/0.1.3#egg=django-holodeck-0.1.3
 $pip install http://github.com/unomena/photon/tarball/0.0.6#egg=photon-0.0.6
 
-$manage syncdb --noinput --no-initial-data --migrate
-$manage collectstatic --noinput
+DJANGO_SETTINGS_MODULE="project.settings" $manage syncdb --noinput --no-initial-data --migrate
+DJANGO_SETTINGS_MODULE="project.settings" $manage collectstatic --noinput
