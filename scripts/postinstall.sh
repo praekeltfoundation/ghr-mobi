03cad58629fb9092ebbbc83ac870a601106f7bb5
Milton Madanda <madandat@gmail.com>
2014-10-14 14:09:04 +0200

    ensure correct version of holodeck is installed

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,9 +4,8 @@ pip="${VENV}/bin/pip"
 $pip install http://github.com/unomena/django-ckeditor-new/tarball/3.6.2.2#egg=django-ckeditor-3.6.2.2
 $pip install http://github.com/unomena/django-photologue/tarball/2.8.praekelt#egg=django-photologue-2.8.praekelt
 $pip install http://github.com/unomena/tunobase/tarball/1.0.10#egg=tunobase-1.0.10
-$pip install http://github.com/unomena/django-holodeck/tarball/0.1.3#egg=django-holodeck-0.1.3
+$pip install http://github.com/smn/django-holodeck/tarball/0.1.4#egg=django-holodeck
 $pip install http://github.com/unomena/photon/tarball/0.0.6#egg=photon-0.0.6
 
-cd "${INSTALLDIR}/ghr-mobi/"
 DJANGO_SETTINGS_MODULE="project.settings" $manage syncdb --noinput --no-initial-data --migrate
 DJANGO_SETTINGS_MODULE="project.settings" $manage collectstatic --noinput
